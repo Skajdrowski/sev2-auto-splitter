0ab1dd0fbf45427999eb6ae2270fa4f354d04281
Skajdrovski <cssteam1337pl@gmail.com>
2025-03-07 19:16:04 +0100

    Rearranged pointers into structs (and other code refactors)

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,8 @@
 
 extern crate alloc;
 
-use alloc::{string::{String, ToString}};
 use asr::{future::sleep, future::retry, settings::Gui, Process};
-use core::time::Duration;
+use core::{str, time::Duration};
 use dlmalloc::GlobalDlmalloc;
 
 #[global_allocator]
@@ -22,22 +21,49 @@ struct Settings {
     Individual_level: bool
 }
 
+struct Addr {
+    startAddress: u32,
+    loadAddress: u32,
+    splashAddress: u32,
+    levelAddress: u32,
+    bulletCamAddress: u32,
+    objectiveAddress: u32,
+    mcAddress: u32
+}
+
+impl Addr {
+    fn original() -> Self {
+        Self {
+            startAddress: 0x689FE2,
+            loadAddress: 0x67FC38,
+            splashAddress: 0x653B40,
+            levelAddress: 0x685F31,
+            bulletCamAddress: 0x65B917,
+            objectiveAddress: 0x656F3C,
+            mcAddress: 0x689FD2
+        }
+    }
+    
+    fn remastered() -> Self {
+        Self {
+            startAddress: 0x799A77,
+            loadAddress: 0x774FE3,
+            splashAddress: 0x74C670,
+            levelAddress: 0x7CFC7D,
+            bulletCamAddress: 0x76DD17,
+            objectiveAddress: 0x7CF568,
+            mcAddress: 0x799A63
+        }
+    }
+}
+
 async fn main() {
     let mut settings = Settings::register();
-    let mut oldLevel = String::new();
+    let mut oldLevel = [0u8; 38];
     let mut oldSplash = 0;
     
-    let mut startAddress = 0x689FE2;
-
-    let mut loadAddress = 0x67FC38;
-    let mut splashAddress = 0x653B40;
-
-    let mut levelAddress = 0x685F31;
-
-    let mut bulletCamAddress = 0x65B917;
-    let mut objectiveAddress = 0x656F3C;
-    
-    let mut mcAddress = 0x689FD2;
+    let mut baseAddress = asr::Address::new(0);
+    let mut addrStruct = Addr::original();
 
     //asr::print_message("Autosplitter for Sniper Elite V2 started!");
 
@@ -46,126 +72,78 @@ async fn main() {
     }).await;
 
     process.until_closes(async {
-        let baseAddress = match process.get_module_address("SniperEliteV2.exe") {
-            Ok(baseAddress) => baseAddress,
-            Err(_) => {
-                match process.get_module_address("SEV2_Remastered.exe") {
-                    Ok(baseAddress) => baseAddress,
-                    Err(_) => return
-                }
+        if let Some((base, moduleSize)) = ["SniperEliteV2.exe", "SEV2_Remastered.exe"].into_iter().find_map (
+        |exe| Some((process.get_module_address(exe).ok()?, process.get_module_size(exe).ok()?))
+        )
+        {
+            baseAddress = base;
+            if moduleSize == 18169856 {
+                addrStruct = Addr::remastered();
             }
-        };
-        
-        let moduleSize = match process.get_module_size("SniperEliteV2.exe") {
-            Ok(moduleSize) => moduleSize,
-            Err(_) => {
-                match process.get_module_size("SEV2_Remastered.exe") {
-                    Ok(moduleSize) => moduleSize,
-                    Err(_) => return
-                }
-            }
-        };
-        if moduleSize == 18169856 {
-            startAddress = 0x799A77;
-
-            loadAddress = 0x774FE3;
-            splashAddress = 0x74C670;
-            
-            levelAddress = 0x7CFC7D;
-            
-            bulletCamAddress = 0x76DD17;
-            objectiveAddress = 0x7CF568;
-            
-            mcAddress = 0x799A63;
         }
 
         let start = || {
-            let startByte = match process.read::<u8>(baseAddress + startAddress) {
-                Ok(startByte) => startByte,
-                Err(_) => return
-            };
-
-            if startByte == 1 {
-                asr::timer::start();
+            if let Ok(startByte) = process.read::<u8>(baseAddress + addrStruct.startAddress) {
+                if startByte == 1 {
+                    asr::timer::start();
+                }
             }
         };
         
         let isLoading = || {
-            let loadByte = match process.read::<u8>(baseAddress + loadAddress) {
-                Ok(loadByte) => loadByte,
-                Err(_) => return
-            };
-            let splashByte = match process.read::<u8>(baseAddress + splashAddress) {
-                Ok(splashByte) => splashByte,
-                Err(_) => return
-            };
-            
-            if loadByte == 0 || splashByte == 0 {
-                asr::timer::pause_game_time();
-            }
-            else {
-                asr::timer::resume_game_time();
+            if let (Ok(loadByte), Ok(splashByte)) = (
+            process.read::<u8>(baseAddress + addrStruct.loadAddress),
+            process.read::<u8>(baseAddress + addrStruct.splashAddress)
+            ) {
+                if loadByte == 0 || splashByte == 0 {
+                    asr::timer::pause_game_time();
+                }
+                else {
+                    asr::timer::resume_game_time();
+                }
             }
         };
 
         let mut levelSplit = || {
-            let levelByte = match process.read_vec(baseAddress + levelAddress, 38) {
-                Ok(levelByte) => levelByte,
-                Err(_) => return
-            };
-            let level = String::from_utf8_lossy(&levelByte).split('\0').next().unwrap_or("").to_string();
-            if level != oldLevel {
-                oldLevel = level.clone();
-                if (level != "nu\\Options.gui" || oldLevel != "nu\\Options.gui") && level != "Tutorial\\M01_Tutorial.pc" {
-                    asr::timer::split();
+            if let Ok(levelByte) = process.read_vec(baseAddress + addrStruct.levelAddress, 38) {
+                if levelByte != oldLevel {
+                    oldLevel.copy_from_slice(&levelByte);
+                    let level = str::from_utf8(&levelByte).unwrap_or("").split('\0').next().unwrap_or("");
+
+                    if (level != "nu\\Options.gui"
+                    || str::from_utf8(&oldLevel).unwrap_or("").split('\0').next().unwrap_or("") != "nu\\Options.gui")
+                    && level != "Tutorial\\M01_Tutorial.pc" {
+                        asr::timer::split();
+                    }
                 }
             }
         };
 
         let lastSplit = || {
-            let lastLevelByte = match process.read_vec(baseAddress + levelAddress, 38) {
-                Ok(lastLevelByte) => lastLevelByte,
-                Err(_) => return
-            };
-            let bulletCamByte = match process.read::<u8>(baseAddress + bulletCamAddress) {
-                Ok(bulletCamByte) => bulletCamByte,
-                Err(_) => return
-            };
-            let objectiveByte = match process.read::<u8>(baseAddress + objectiveAddress) {
-                Ok(objectiveByte) => objectiveByte,
-                Err(_) => return
-            };
-
-            let lastLevel = String::from_utf8_lossy(&lastLevelByte).split('\0').next().unwrap_or("").to_string();
-            if lastLevel == "BrandenburgGate\\M11_BrandenburgGate.pc" && bulletCamByte == 1 && objectiveByte == 3 {
-                asr::timer::split();
+            if let (Ok(lastLevelByte), Ok(bulletCamByte), Ok(objectiveByte)) = (
+            process.read_vec(baseAddress + addrStruct.levelAddress, 38),
+            process.read::<u8>(baseAddress + addrStruct.bulletCamAddress),
+            process.read::<u8>(baseAddress + addrStruct.objectiveAddress)) {
+                if str::from_utf8(&lastLevelByte).unwrap_or("").split('\0').next().unwrap_or("") == "BrandenburgGate\\M11_BrandenburgGate.pc"
+                && bulletCamByte == 1
+                && objectiveByte == 3 {
+                    asr::timer::split();
+                }
             }
         };
 
         let mut individualLvl = || {
-            let ilSplashByte = match process.read::<u8>(baseAddress + splashAddress) {
-                Ok(ilSplashByte) => ilSplashByte,
-                Err(_) => return
-            };
-            let mcByte = match process.read::<u8>(baseAddress + mcAddress) {
-                Ok(mcByte) => mcByte,
-                Err(_) => return
-            };
-            let ilLevelByte = match process.read_vec(baseAddress + levelAddress, 38) {
-                Ok(ilLevelByte) => ilLevelByte,
-                Err(_) => return
-            };
-            let ilLevel = String::from_utf8_lossy(&ilLevelByte).split('\0').next().unwrap_or("").to_string();
+            if let (Ok(ilSplashByte), Ok(mcByte), Ok(ilLevelByte)) = (process.read::<u8>(baseAddress + addrStruct.splashAddress), process.read::<u8>(baseAddress + addrStruct.mcAddress), process.read_vec(baseAddress + addrStruct.levelAddress, 38)) {
+                if mcByte == 1 {
+                    asr::timer::split();
+                }
 
-            if mcByte == 1 {
-                asr::timer::split();
-            }
-
-            if (ilSplashByte == 1 && oldSplash == 0) && ilLevel != "nu\\Options.gui" {
-                asr::timer::start();
-            }
-            if ilSplashByte != oldSplash {
-                oldSplash = ilSplashByte;
+                if ilSplashByte != oldSplash {
+                    oldSplash = ilSplashByte;
+                }
+                if (ilSplashByte == 1 && oldSplash == 0) && str::from_utf8(&ilLevelByte).unwrap_or("").split('\0').next().unwrap_or("") != "nu\\Options.gui" {
+                    asr::timer::start();
+                }
             }
         };
 
@@ -181,7 +159,7 @@ async fn main() {
             }
             isLoading();
 
-            sleep(Duration::from_micros(16666)).await;
+            sleep(Duration::from_nanos(16666666)).await;
         }
     }).await;
 }
